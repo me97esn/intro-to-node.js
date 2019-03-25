ac5fdd556c38ede3ba6ccbf74a88946bb5bda3e8
Emil <emilsten@kth.se>
2019-03-25 14:36:59 +0100

    use try catch for async errors :)

--- error_handling/5-request-async-await.js
+++ error_handling/5-request-async-await.js
@@ -1,8 +1,20 @@
+const express = require('express')
+const app = express()
+const request = require('request')
 const { promisify } = require('util')
-const request = promisify(require('request'))
-async function getQuote() {
-	const quote =  await request.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand').json()
-	console.log(quote)
+const get = promisify(request.get)
+async function index (req, response) {
+    try {
+        const quoteResponse =  await get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand')
+        const json = JSON.parse(quoteResponse.body)
 
+        response.send(json[0].content)
+
+    } catch (error) {
+        console.log(error)
+        response.send(error.message)
+    }
 }
-getQuote()
+app.get('/', index)
+console.log('listening on localhost:3000')
+app.listen(3000)
